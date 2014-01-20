82bccf2fca9ee4c9d1b3226c30f02f2362ae3954
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-01-20 17:39:44 +0900

    PointとFaceConnectsを追加

--- array_maker.py
+++ array_maker.py
@@ -2,5 +2,22 @@
 import maya.OpenMaya
 import maya.OpenMayaMPx
 
-class VertexMaker:
-  def 
+class ArrayMaker:
+  def __init__(self):
+    pass
+
+  def MakePoints(self, array):
+    points = maya.OpenMaya.MFloatPointArray()
+    points.setLength(len(array))
+    for i in range(len(array)):
+      vtx = maya.OpenMaya.MFloatPoint(array[i][0], array[i][1], array[i][2])
+      points.set(vtx ,i)
+    return points
+
+  def MakeFaceConnects(self, array):
+    connects = maya.OpenMaya.MIntArray()
+    connects.setLength(len(array)*3)
+    for i in range(len(array)):
+      for j in range(3):
+        connects[i*3+j] = array[i*3+j]
+    return connects
