212adb07afc4d487e4d9698a49a13999102082c7
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-15 18:11:50 +0900

    add check element

--- material_importer.py
+++ material_importer.py
@@ -1,11 +1,11 @@
 #-*- encoding: utf-8
 class Material:
   def __init__(self, splited):
-    self.diffuse_color = self.to_floats(splited[2:5])
-    self.reflect_color = self.to_floats(splited[6:8])
-    self.reflectivity  = float(splited[9])
-    self.ambient_color = self.to_floats(splited[10:12])
-    self.texture = splited[25]
+    self.diffuse_color = self.to_floats(splited[3:6])
+    self.reflect_color = self.to_floats(splited[7:9])
+    self.reflectivity  = float(splited[10])
+    self.ambient_color = self.to_floats(splited[11:13])
+    self.texture = splited[26]
 
   def to_floats(self, arr):
     retarr = []
@@ -16,9 +16,14 @@ class Material:
 
 class MaterialImporter:
   def __init__(self):
-    f = open("c:/out_material.csv")
-    dic = {}
-    for line in f:
-      splited = f.split(',')
-      mat_name = splited[0]
-      dic[mat_name] = Material(splited)
+    pass
+
+  def import_csv(self):
+    with open("c:/out_material.csv") as f:
+      dic = {}
+      for line in f:
+        splited = f.split(',')
+        if splited[0] == "Material":
+          mat_name = splited[1]
+          dic[mat_name] = Material(splited)
+    return dic
