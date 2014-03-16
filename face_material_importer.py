ef92fb1a04bd2916e2831e36a27c448fb848a049
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-17 01:14:24 +0900

    modify material importer

--- face_material_importer.py
+++ face_material_importer.py
@@ -1,33 +1,14 @@
 #-*- encoding: utf-8
 
-class FaceMaterial:
-  def __init__(self, splited):
-    self.material = splited[1]
-    self.index = int(splited[2])
-
-
-class FaceMaterialImporter:
-  def __init__(self, directory):
-    self.directory = directory
-
-  def importCSV(self, lines):
-    faces = []
-    for splited in lines:
-      if splited[0] == "Face":  # 材質ごとに;Face行が存在するためそれは無視する
-        self.faces.append(FaceMaterial(splited))
-    return faces
-
-
 class FaceMaterialGenerator:
-  def __init__(self, directory):
-    self.importer = FaceMaterialImporter(directory)
+  def __init__(self):
+    pass
 
-  def generate(self, model, shader_groups):
-    faces = self.importer.importCSV()
-    self.setMaterial(model, shader_groups, faces)
-
-  def setMaterial(self, model, shader_groups, faces):
-    for f in faces:
-      num = str(f.index)
-      target_sg = shader_groups[f.material]
-      cmds.sets("%s.f[%s]" % (model, num), e=1, forceElement=target_sg)
+  def generate(self, records, model, shader_groups):
+    cnt = 0
+    for row in records:
+      if row[0] == "Face":
+        mat_name = row[1]
+        sg = shader_groups[mat_name]
+        cmds.sets("%s.f[%s]" % (model, cnt), e=1, forceElement=sg)
+        cnt += 1
