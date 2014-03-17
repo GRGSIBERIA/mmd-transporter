0d129f1c2d9cf1560de2635c4d0c2df539460087
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-18 03:34:28 +0900

    debug load textur

--- material_importer.py
+++ material_importer.py
@@ -1,5 +1,6 @@
 #-*- encoding: utf-8
 import os
+import maya.cmds as cmds
 
 class Material:
   def __init__(self, rows):
@@ -31,23 +32,23 @@ class MaterialImporter:
 
 
 class MaterialGenerator:
-  def __init__(self, csv_file_path):
+  def __init__(self, records, csv_file_path):
     self.directory = os.path.dirname(csv_file_path)
-    self.importer = MaterialImporter(directory)
-    self.material_dict = self.importer.importCSV()
+    self.importer = MaterialImporter()
+    self.material_dict = self.importer.importCSV(records)
 
   # ShaderGroupを返す，SGはcmds.setsで利用する
   def generate(self):
     shader_group_for_mesh = {}
     for name, mat in self.material_dict.items():
-      material_node, shader_group = self.createNode(model, name)
+      material_node, shader_group = self.createNode(name)
       file_node = self.createTexture()
       self.setTexture(file_node, mat)
       self.setMaterial(material_node, file_node, mat)
       shader_group_for_mesh[name] = shader_group
     return shader_group_for_mesh
 
-  def createNode(self, model, name):
+  def createNode(self, name):
     material = cmds.shadingNode("blinn", asShader=1)
     shader_group = cmds.sets(renderable=1, noSurfaceShader=1, empty=1, name='%sSG' % material)
     #cmds.sets(model, e=1, forceElement=shader_group)   # ここは無視しておこう
@@ -78,15 +79,20 @@ class MaterialGenerator:
     return file_node
 
   def setTexture(self, file_node, material):
-    cmds.setAttr("%s.fileTextureName" % file_node, self.directory + material.texture, type="string")
+    print self.directory + "/" + material.texture
+    cmds.setAttr("%s.fileTextureName" % file_node, self.directory + "/" + material.texture, type="string")
 
   def setMaterial(self, mat_node, file_node, material):
-    cmds.setAttr("%s.color" % mat_node, material.diffuse_color[0], material.diffuse_color[1], material.diffuse_color[2], type="double3")
     cmds.setAttr("%s.transparency" % mat_node, material.transparent, material.transparent, material.transparent, type="double3")
     cmds.setAttr("%s.ambientColor" % mat_node, material.ambient_color[0], material.ambient_color[1], material.ambient_color[2], type="double3")
     cmds.setAttr("%s.specularColor" % mat_node, material.specular_color[0], material.specular_color[1], material.specular_color[2], type="double3")
     cmds.setAttr("%s.eccentricity" % mat_node, material.specularity)
     cmds.setAttr("%s.specularRollOff" % mat_node, material.specularity)
+
+    if material.texture != "":
+      cmds.connectAttr("%s.outColor" % file_node ,"%s.color" % mat_node)
+    else:
+      cmds.setAttr("%s.color" % mat_node, material.diffuse_color[0], material.diffuse_color[1], material.diffuse_color[2], type="double3")
 
     ext = os.path.splitext(material.texture)[1]
     if material.transparent > 0.0 and (ext == ".png" or ext == ".tga"):
