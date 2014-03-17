2a84525819d0d049d32261bc5ce2f2ed910c1852
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-18 02:02:58 +0900

    refactoring

--- loadmmd_command.py
+++ loadmmd_command.py
@@ -1,4 +1,8 @@
 #-*- encoding: utf-8
+import maya.OpenMaya
+import maya.OpenMayaMPx
+import maya.cmds as cmds
+
 class LoadMMDCommand(maya.OpenMayaMPx.MPxCommand):
   def __init__(self):
     maya.OpenMayaMPx.MPxCommand.__init__(self)
@@ -8,8 +12,10 @@ class LoadMMDCommand(maya.OpenMayaMPx.MPxCommand):
     return maya.OpenMayaMPx.asMPxPtr(cls())
 
   def doIt(self, args):
+    csv_file_path = cmds.fileDialog2(dialogStyle=2, selectFileFilter="*.csv", okCaption="Select")[0]
+
     poly = maya.cmds.createNode('transform')
     mesh = maya.cmds.createNode('mesh', parent=poly)
     maya.cmds.sets(mesh, add='initialShadingGroup')
     spoly = maya.cmds.createNode('transportedMMD1')
-    maya.cmds.connectAttr(spoly + '.outputMesh', mesh + '.inMesh') 
+    maya.cmds.connectAttr(spoly + '.outputMesh', mesh + '.inMesh')
--- main.py
+++ main.py
@@ -17,10 +17,27 @@ kPluginNodeId = maya.OpenMaya.MTypeId(0x03939)
 kPluginCmdName = "loadmmd"
 
 
+def nodeCreator():
+  csv_file_path = cmds.fileDialog2(dialogStyle=2, selectFileFilter="*.csv", okCaption="Select")[0]
+  return maya.OpenMayaMPx.asMPxPtr(MMDTransporter(csv_file_path))
+
+
+def nodeInitializer():
+  nAttr = maya.OpenMaya.MFnNumericAttribute()
+  MMDTransporter.widthHeight = nAttr.create('widthHeight', 'wh', maya.OpenMaya.MFnNumericData.kFloat, 1.0)
+  nAttr.setStorable(1)
+
+  typedAttr = maya.OpenMaya.MFnTypedAttribute()
+  MMDTransporter.outputMesh = typedAttr.create('outputMesh', 'out', maya.OpenMaya.MFnData.kMesh)
+  MMDTransporter.addAttribute(MMDTransporter.widthHeight)
+  MMDTransporter.addAttribute(MMDTransporter.outputMesh)
+  MMDTransporter.attributeAffects(MMDTransporter.widthHeight, MMDTransporter.outputMesh)
+
+
 def initializePlugin(mobject):
   mplugin = maya.OpenMayaMPx.MFnPlugin(mobject, "Eiichi Takebuchi", "1.0")
   try:
-    mplugin.registerNode(kPluginNodeName, kPluginNodeId, MMDTransporter.nodeCreator, MMDTransporter.nodeInitializer)
+    mplugin.registerNode(kPluginNodeName, kPluginNodeId, nodeCreator, nodeInitializer)
     mplugin.registerCommand(kPluginCmdName, LoadMMDCommand.cmdCreator)
   except:
     sys.stderr.write('Failed to register node: %s' % kPluginNodeName)
@@ -35,3 +52,14 @@ def uninitializePlugin(mobject):
   except:
     sys.stderr.write('Failed to deregister node: %s' % kPluginNodeName)
     raise
+
+
+
+
+
+def loadMMDTransporter():
+  poly = maya.cmds.createNode('transform')
+  mesh = maya.cmds.createNode('mesh', parent=poly)
+  maya.cmds.sets(mesh, add='initialShadingGroup')
+  spoly = maya.cmds.createNode('transportedMMD1')
+  maya.cmds.connectAttr(spoly + '.outputMesh', mesh + '.inMesh') 
