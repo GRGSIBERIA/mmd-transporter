5d4b551e8d36692614d50824ba37b15738e38915
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-18 20:35:16 +0900

    add bone_importer

---
+++ bone_importer.py
@@ -0,0 +1,40 @@
+#-*- encoding: utf-8
+import maya.cmds as cmds
+
+class Bone:
+  def __init__(self, number, bone_name, maya_name, abs_pos, rel_pos, parent_bone_name):
+    self.number = number
+    self.bone_name = bone_name
+    self.maya_name = maya_name
+    self.abs_pos = abs_pos
+    self.rel_pos = rel_pos
+    self.parent_bone_name = parent_bone_name
+
+class BoneImporter:
+  def __init__(self):
+    pass
+
+  def importCSV(self, records):
+    bones = {}
+    cnt = 0
+    for rows in records:
+      if rows[0] == "Bone":
+        bone_name = rows[1] # テーブルで英文字に変換
+        apos = self._to_float3(rows, 5)
+        rpos = self._to_float3(rows, 16)
+        bones Bone(cnt, rows[1], bone_name, apos, rpos, rows[13])
+        cnt += 1
+    return bones
+
+  def _to_float3(self, rows, start):
+    return [float(rows[start]), float(rows[start+1]), float(rows[start+2])]
+
+class BoneGenerator:
+  def __init__(self):
+    pass
+
+  def generate(self, records):
+    bones = BoneImporter().importCSV(records)
+    for bname, bone in bones.items():
+      cmds.select(d=True)
+      cmds.joint(p=bone.abs_pos, name=bone.maya_name)
