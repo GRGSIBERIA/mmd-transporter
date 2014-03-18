a4015b6e9a6c0d9b9417563a77e77e7290a2a275
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-18 15:30:36 +0900

    load faster

--- face_material_importer.py
+++ face_material_importer.py
@@ -7,9 +7,19 @@ class FaceMaterialGenerator:
 
   def generate(self, records, model, shader_groups):
     cnt = 0
+    mat_dict = {}
+
+    for mat_name in shader_groups.keys():
+      mat_dict[mat_name] = [] # 配列の初期化
+
     for row in records:
       if row[0] == "Face":
         mat_name = row[1]
-        sg = shader_groups[mat_name]
-        cmds.sets("%s.f[%s]" % (model, cnt), e=1, forceElement=sg)
+        mat_dict[mat_name].append(cnt)  # カウントを入れていく
         cnt += 1
+
+    for mat_name, counts in mat_dict.items():
+      start = counts[0]
+      end = counts[len(counts)-1]
+      sg = shader_groups[mat_name]
+      cmds.sets("%s.f[%s:%s]" % (model, start, end), e=1, forceElement=sg)
