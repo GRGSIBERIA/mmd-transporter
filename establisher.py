291f5530c8964bc5853dab1c283a8e27cde540c1
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-21 15:49:17 +0900

    implemented expressions

--- establisher.py
+++ establisher.py
@@ -4,6 +4,7 @@ import maya.cmds as cmds
 class Axis:
   @classmethod
   def setAxisJoint(cls, bone, axis):
+    cmds.select(bone.maya_name)
     axis_joint = cmds.joint(r=True, p=axis)
     cmds.joint(bone.maya_name, e=True, zso=True, oj="xyz", sao="yup")
     cmds.delete(axis_joint)
@@ -49,15 +50,16 @@ class AttributeEstablisher:
 
   def _createExpression(self, bone_inst, bones, attr_type):
     parent_joint = bones[bone_inst.establish_parent]
-    self._asyncOrientJointFromParent(bone_inst, parent_joint)
+    pbuf = bones[bone_inst.parent_bone_name]
+    self._asyncOrientJointFromParent(bone_inst, pbuf)
     script = self._createScript(bone_inst, bones, attr_type, parent_joint)
     name = "establish_%s_for_%s_from_%s" % (attr_type, bone_inst.maya_name, parent_joint.maya_name)
     cmds.expression(s=script, n=name)
 
   def _asyncOrientJointFromParent(self, bone, parent_joint):
-    axis = [0.0, 0.0, 0.0]
+    axis = []
     for i in range(3):
-      axis = bone.abs_pos[i] - parent_joint.abs_pos[i]
+      axis.append(bone.abs_pos[i] - parent_joint.abs_pos[i])
     Axis.setAxisJoint(bone, axis)
 
   def _createScript(self, bone_inst, bones, attr_type, parent_joint):
