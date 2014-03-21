00551079ce53d099127d76cc9b5b8cba0cc15fb6
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-21 14:54:37 +0900

    add async axis for expression

--- establisher.py
+++ establisher.py
@@ -1,15 +1,20 @@
 #-*- encoding: utf-8
 import maya.cmds as cmds
 
+class Axis:
+  @classmethod
+  def setAxisJoint(cls, bone, axis):
+    axis_joint = cmds.joint(r=True, p=axis)
+    cmds.joint(bone.maya_name, e=True, zso=True, oj="xyz", sao="yup")
+    cmds.delete(axis_joint)
+
 class AxisLimitter:
   def __init__(self):
     pass
 
   def giveAxis(self, bone):
     if bone.enable_axis == 1:
-      axis_joint = cmds.joint(r=True, p=bone.limit_axis)
-      cmds.joint(bone.maya_name, e=True, zso=True, oj="xyz", sao="yup")
-      cmds.delete(axis_joint)
+      Axis.setAxisJoint(bone, bone.limit_axis)
       cmds.setAttr("%s.jointTypeY" % bone.maya_name, 0)
       cmds.setAttr("%s.jointTypeZ" % bone.maya_name, 0)
       cmds.setAttr("%s.ry" % bone.maya_name, lock=True)
@@ -44,9 +49,16 @@ class AttributeEstablisher:
 
   def _createExpression(self, bone_inst, bones, attr_type):
     parent_joint = bones[bone_inst.establish_parent]
+    self._asyncOrientJointFromParent(bone_inst, parent_joint)
     script = self._createScript(bone_inst, bones, attr_type, parent_joint)
     name = "establish_%s_for_%s_from_%s" % (attr_type, bone_inst.maya_name, parent_joint.maya_name)
     cmds.expression(s=script, n=name)
+
+  def _asyncOrientJointFromParent(self, bone, parent_joint):
+    axis = [0.0, 0.0, 0.0]
+    for i in range(3):
+      axis = bone.abs_pos[i] - parent_joint.abs_pos[i]
+    Axis.setAxisJoint(bone, axis)
 
   def _createScript(self, bone_inst, bones, attr_type, parent_joint):
     script = ""
