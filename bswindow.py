2c2ba5cdf7e8adf685c17afcceba60685012cd0f
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-05-20 04:02:41 +0900

    BlendShape用の窓作る

--- bswindow.py
+++ bswindow.py
@@ -6,15 +6,22 @@ import util
 
 class MmdBlendShapeWindow:
   def __init__(self):
-    pass
-
-  def _layout(self, window):
-    maya.cmds.columnLayout()
+    try:
+      self.transform = maya.cmds.ls(sl=True)[0]
+    except:
+      raise Exception("Do not select MMD transform object.")
+    self.blendShapeNode = self._getBlendShape(self.transform)
+    self.blendShapeWeights = self._getWeights(self.blendShapeNode)
+    self.blendShapeGroups = self._getBlendShapeGroups(self.blendShapeNode, self.transform)
+    self.blendShapeNames = self._getBlendShapeNames(self.blendShapeGroups)
 
 
   def _getBlendShape(self, transform):
     histories = maya.cmds.listHistory(transform)
-    blendShape = maya.cmds.ls(histories, type="blendShape")[0]
+    try:
+      blendShape = maya.cmds.ls(histories, type="blendShape")[0]
+    except:
+      raise Exception("Do not have blendShape node selected transform.")
     return blendShape
 
 
@@ -56,17 +63,57 @@ class MmdBlendShapeWindow:
     return blendShapeNames
 
 
+  def _callSelectBlendShapeNode(self):
+    maya.cmds.select(self.blendShapeNode)
+
+
+  def _menu(self):
+    maya.cmds.menu(l="Option")
+    maya.cmds.menuItem(l="Select BlendShape Node",\
+      command=lambda *args:self._callSelectBlendShapeNode())
+
+  def _changeFloatSlider(self, shape, floatValue):
+    blendShapeName = "%s.%s" % (self.blendShapeNode, shape)
+    maya.cmds.setAttr(blendShapeName, floatValue)
+
+
+  def _setKeyFrame(self, shape):
+    maya.cmds.setKeyframe("%s.%s" % (self.blendShapeNode, shape))
+
+
+  def _drawBlendShapeLine(self, shape, jpName):
+    floatValue = maya.cmds.getAttr("%s.%s" % (self.blendShapeNode, shape))
+    maya.cmds.floatSliderGrp(\
+      l=jpName, min=0, max=1, step=0.02, fmn=0, fmx=1, fs=0.02, \
+      width=300, value=floatValue, field=True,\
+      changeCommand=lambda *args:self._changeFloatSlider(shape, args[0]))
+    maya.cmds.button(l="Key", w=48, command=lambda *args:self._setKeyFrame(shape))
+
+
+  def _layout(self, window, blendShapeNames):
+    #form = maya.cmds.formLayout(numberOfDivisions=100)
+    scrollLayout = maya.cmds.scrollLayout(vst=16)
+    maya.cmds.columnLayout(adj=True)
+    for expType, shapes in blendShapeNames.items():
+      maya.cmds.frameLayout(collapsable=True, l=expType)
+      maya.cmds.rowColumnLayout(nc=2)
+      for shape, jpName in shapes.items():
+        self._drawBlendShapeLine(shape, jpName)
+      maya.cmds.setParent("..")
+      maya.cmds.setParent("..")
+
+
   def show(self):
-    window = maya.cmds.window(t="MMD BlendShape Window", wh=(200, 600))
+    title = "MMD BlendShape Window"
+    window = maya.cmds.window(\
+      t=title, wh=(400, 800),\
+      menuBar=True)
 
-    transform = maya.cmds.ls(sl=True)[0]
-    blendShapeNode = self._getBlendShape(transform)
-    blendShapeWeights = self._getWeights(blendShapeNode)
-    blendShapeGroups = self._getBlendShapeGroups(blendShapeNode, transform)
-    blendShapeNames = self._getBlendShapeNames(blendShapeGroups)
+    self._menu()
+    self._layout(window, self.blendShapeNames)
+    #maya.cmds.showWindow()
+    maya.cmds.dockControl(\
+      l=title, content=window, area="left", allowedArea=("left", "right"))
 
-    self._layout(window)
-    maya.cmds.showWindow()
-
-#w = MmdBlendShapeWindow()
-#w.show()
+w = MmdBlendShapeWindow()
+w.show()
