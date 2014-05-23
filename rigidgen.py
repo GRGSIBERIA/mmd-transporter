63c6345f31ed94f43c0658cb144ff03fc1df922a
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-05-24 00:24:44 +0900

    jointが暴れる……

--- rigidgen.py
+++ rigidgen.py
@@ -18,20 +18,27 @@ class RigidBodyGenerator:
     self.constPI = 180.0 / 3.141592653589793
 
 
-  def _setPosture(self, shape, rigid):
+  def _createPostureCube(self, rigid):
+    pCube = maya.cmds.polyCube()[0]
+
     pos = rigid.shape_position
-    maya.cmds.setAttr("%s.initialTranslateX" % shape,  pos.x)
-    maya.cmds.setAttr("%s.initialTranslateY" % shape,  pos.y)
-    maya.cmds.setAttr("%s.initialTranslateZ" % shape, -pos.z)
+    maya.cmds.setAttr("%s.translateX" % pCube,  pos.x)
+    maya.cmds.setAttr("%s.translateY" % pCube,  pos.y)
+    maya.cmds.setAttr("%s.translateZ" % pCube, -pos.z)
     
     rot = rigid.shape_rotation
-    rot = maya.OpenMaya.MEulerRotation(-rot.x, rot.y, rot.z, 4) #kYXZ
+    rot = maya.OpenMaya.MEulerRotation(-rot.x, rot.y, rot.z, 2) #kYXZ
     quat = rot.asQuaternion()
     rot = quat.asEulerRotation()
-    print rot.order
-    maya.cmds.setAttr("%s.initialRotateX" % shape, rot.x)
-    maya.cmds.setAttr("%s.initialRotateY" % shape, rot.y)
-    maya.cmds.setAttr("%s.initialRotateZ" % shape, rot.z)
+    maya.cmds.setAttr("%s.rotateX" % pCube, rot.x * self.constPI)
+    maya.cmds.setAttr("%s.rotateY" % pCube, rot.y * self.constPI)
+    maya.cmds.setAttr("%s.rotateZ" % pCube, rot.z * self.constPI)
+
+    maya.cmds.setAttr("%s.scaleX" % pCube, 0.0001)
+    maya.cmds.setAttr("%s.scaleY" % pCube, 0.0001)
+    maya.cmds.setAttr("%s.scaleZ" % pCube, 0.0001)
+    maya.cmds.makeIdentity(apply=True, s=1)
+    return pCube
 
 
   def _setColliderSize(self, shape, rigid):
@@ -51,157 +58,129 @@ class RigidBodyGenerator:
       maya.cmds.setAttr("%s.length" % shape, shapeSize[1] + shapeSize[0] * 2)
 
 
-  def _createRigidbodies(self):
+  def _setParams(self, shape, param):
+    maya.cmds.setAttr("%s.mass" % shape, param.mass)
+    maya.cmds.setAttr("%s.linearDamping" % shape, param.linear_damping)
+    maya.cmds.setAttr("%s.friction" % shape, param.friction)
+    maya.cmds.setAttr("%s.angularDamping" % shape, param.angular_damping)
+    maya.cmds.setAttr("%s.restitution" % shape, param.restitution)
+
+
+  def _parentConstraint(self, a, b):
+    maya.cmds.select(a)
+    maya.cmds.select(b, tgl=True)
+    maya.cmds.parentConstraint(mo=True)
+
+
+  def _constraintRigidbody(self, shape, rigid, jointNames):
+    targetJoint = ""
+    if rigid.bone_index >= 0 and len(jointNames) > rigid.bone_index:
+      targetJoint = jointNames[rigid.bone_index]
+
+    if targetJoint != "":
+      if rigid.mode == 0:   # ボーン追従
+        maya.cmds.setAttr("%s.bodyType" % shape, 1)
+        maya.cmds.parentConstraint(targetJoint, shape, mo=True)
+
+      elif rigid.mode == 1: # 物理演算
+        maya.cmds.setAttr("%s.bodyType" % shape, 2)
+        #maya.cmds.parentConstraint(shape, targetJoint, mo=True)
+        maya.cmds.pointConstraint(targetJoint, shape, mo=True)
+        maya.cmds.orientConstraint(shape, targetJoint, mo=True)
+
+      elif rigid.mode == 2: # 位置合わせ
+        maya.cmds.setAttr("%s.bodyType" % shape, 2)
+        maya.cmds.pointConstraint(targetJoint, shape, mo=True)
+        maya.cmds.orientConstraint(shape, targetJoint, mo=True)
+
+
+  def _setCollisionFilter(self, shape, rigid):
+    collisionGroup = 2**rigid.collision_group
+    noCollisionGroup = (-rigid.no_collision_group - 1) ^ 0xFFFF
+    maya.cmds.setAttr("%s.collisionFilterGroup" % shape, collisionGroup)
+    maya.cmds.setAttr("%s.collisionFilterMask" % shape, noCollisionGroup)
+
+
+  def _createRigidbodies(self, jointNames):
     rigids = self.mmdData.rigidbodies
     shapes = []
-    for rigid in rigids:
+    for i in range(len(rigids)):
+      rigid = rigids[i]
       maya.cmds.select(d=True)
+
+      pCube = self._createPostureCube(rigid)
+      pCube = maya.cmds.rename(pCube, "rcube_" + self.nameDict[i])
+      maya.cmds.select(pCube)
       shape = bullet.RigidBody.CreateRigidBody().executeCommandCB()[0]
-      maya.cmds.rename(shape, self.nameDict[rigid.name])
+      shape = maya.cmds.rename(shape, "rigid_" + self.nameDict[i])
       util.setJpName(shape, rigid.name)
-      self._setPosture(shape, rigid)
+      
       self._setColliderSize(shape, rigid)
+      self._setParams(shape, rigid.param)
+      self._constraintRigidbody(shape, rigid, jointNames)
+      self._setCollisionFilter(shape, rigid)
       shapes.append(shape)
     return shapes
 
 
+  def _instantiateJoint(self, joint, shapes):
+    ai = joint.rigidbody_index_a
+    bi = joint.rigidbody_index_b
+    maya.cmds.select(shapes[ai])
+    maya.cmds.select(shapes[bi], tgl=True)
+    constraint = bullet.RigidBodyConstraint.CreateRigidBodyConstraint().executeCommandCB()[0]
+    maya.cmds.setAttr("%s.constraintType" % constraint, 5)
+    return constraint
+
+  def _constraintJointWithRigidbody(self, constraint, joint, jointNames):
+    ai = joint.rigidbody_index_a
+    bi = self.mmdData.rigidbodies[ai].bone_index
+    parentBoneName = jointNames[bi]
+    try:
+      maya.cmds.parentConstraint(parentBoneName, constraint, mo=True)
+    except:
+      print (parentBoneName, constraint)
+
+
+  def _setJointLimitation(self, constraint, minVector, maxVector, limitType, axis, i):
+    args = (constraint, limitType, axis)
+    minValue = minVector[i]
+    maxValue = maxVector[i]
+
+    if minVector[i] > maxVector[i]:
+      maya.cmds.setAttr("%s.%sConstraint%s" % args, 0)
+    if minVector[i] == 0.0 and maxVector[i] == 0.0:
+      maya.cmds.setAttr("%s.%sConstraint%s" % args, 1)
+    else:
+      maya.cmds.setAttr("%s.%sConstraint%s" % args, 2)
+      maya.cmds.setAttr("%s.%sConstraintMin%s" % args, minValue)
+      maya.cmds.setAttr("%s.%sConstraintMax%s" % args, maxValue)
+
+
+  def _setSpringLimitation(self, constraint, limitVector, limitType, axis, i):
+    limitValue = limitVector[i] #self._convertCoordinate(limitVector[i], limitType, axis)
+    args = (constraint, limitType, axis)
+    if limitValue > 0.0 or limitValue < 0.0:
+      maya.cmds.setAttr("%s.%sSpringEnabled%s" % args, 1)
+    maya.cmds.setAttr("%s.%sSpringStiffness%s" % args, limitValue)
+
+
+  def _createJoints(self, shapes, jointNames):
+    joints = self.mmdData.joints
+    for i in range(len(joints)):
+      joint = joints[i]
+      constraint = self._instantiateJoint(joint, shapes)
+      self._constraintJointWithRigidbody(constraint, joint, jointNames)
+
+      axis = ["X", "Y", "Z"]
+      for i in range(3):
+        self._setJointLimitation(constraint, joint.translation_limit_min, joint.translation_limit_max, "linear", axis[i], i)
+        self._setJointLimitation(constraint, joint.rotation_limit_min, joint.rotation_limit_max, "angular", axis[i], i)
+        self._setSpringLimitation(constraint, joint.spring_constant_translation, "linear", axis[i], i)
+        self._setSpringLimitation(constraint, joint.spring_constant_rotation, "angular", axis[i], i)
+
+
   def generate(self, jointNames):
-    rigidShapes = self._createRigidbodies()
+    rigidShapes = self._createRigidbodies(jointNames)
+    self._createJoints(rigidShapes, jointNames)
 
-
-
-  # def _initializePose(self, rigidObj, rigid):
-  #   util.setJpName(rigidObj, rigid.name)
-  #   pos = rigid.shape_position
-  #   rot = rigid.shape_rotation
-  #   maya.cmds.move(pos.x, pos.y, -pos.z)
-  #   maya.cmds.rotate(-rot.x * self.constPI, rot.y * self.constPI, rot.z * self.constPI)
-  #   maya.cmds.scale(0.01, 0.01, 0.01)
-  #   maya.cmds.makeIdentity(apply=True, s=1)
-  #   #maya.cmds.setAttr("%s.v" % rigidObj, 0)
-  #   shape = bullet.RigidBody.CreateRigidBody().executeCommandCB()[1]
-  #   return shape
-
-
-  # def _initializeParams(self, shape, param):
-  #   maya.cmds.setAttr("%s.mass" % shape, param.mass)
-  #   maya.cmds.setAttr("%s.linearDamping" % shape, param.linear_damping)
-  #   maya.cmds.setAttr("%s.friction" % shape, param.friction)
-  #   maya.cmds.setAttr("%s.angularDamping" % shape, param.angular_damping)
-  #   maya.cmds.setAttr("%s.restitution" % shape, param.restitution)
-
-
-  # def _initializeColider(self, shape, rigid):
-  #   maya.cmds.setAttr("%s.autoFit" % shape, 0)
-  #   shapeSize = rigid.shape_size
-  #   if rigid.shape_type == 0:   # sphere
-  #     maya.cmds.setAttr("%s.colliderShapeType" % shape, 2)
-  #     maya.cmds.setAttr("%s.radius" % shape, shapeSize[0])
-  #   elif rigid.shape_type == 1: # box
-  #     maya.cmds.setAttr("%s.colliderShapeType" % shape, 1)
-  #     maya.cmds.setAttr("%s.extentsX" % shape, shapeSize[0])
-  #     maya.cmds.setAttr("%s.extentsY" % shape, shapeSize[1])
-  #     maya.cmds.setAttr("%s.extentsZ" % shape, shapeSize[2])
-  #   elif rigid.shape_type == 2: # capsule
-  #     maya.cmds.setAttr("%s.colliderShapeType" % shape, 3)
-  #     maya.cmds.setAttr("%s.radius" % shape, shapeSize[0])
-  #     maya.cmds.setAttr("%s.length" % shape, shapeSize[1])
-
-
-  # def _parentConstraint(self, a, b):
-  #   maya.cmds.select(a)
-  #   maya.cmds.select(b, tgl=True)
-  #   maya.cmds.parentConstraint(mo=True)
-
-
-  # def _constructConstraint(self, bones, rigidObj, shape, rigid, jointNames):
-  #   parentJoint = ""
-  #   if rigid.bone_index != -1:
-  #     parentJoint = jointNames[rigid.bone_index]
-  #   mode = rigid.mode
-
-  #   if parentJoint != "":
-  #     if mode == 0:   # ボーン追従
-  #       maya.cmds.setAttr("%s.bodyType" % shape, 0)
-  #       current = bones[rigid.bone_index]
-  #       parentJoint = jointNames[current.parent_index]
-  #       self._parentConstraint(parentJoint, rigidObj)
-  #     elif mode == 1: # 物理演算
-  #       self._parentConstraint(rigidObj, parentJoint)
-  #     elif mode == 2: # 物理＋ボーン位置合わせ
-  #       self._parentConstraint(rigidObj, parentJoint)
-
-
-  # def _settingCollision(self, shape, rigid):
-  #   collisionGroup = 2**rigid.collision_group
-  #   noCollisionGroup = -rigid.no_collision_group - 1
-  #   maya.cmds.setAttr("%s.collisionFilterGroup" % shape, collisionGroup)
-  #   maya.cmds.setAttr("%s.collisionFilterMask" % shape, noCollisionGroup)
-
-  
-  # def _createRigidObjects(self, jointNames):
-  #   rigids = self.mmdData.rigidbodies
-  #   joints = self.mmdData.joints
-  #   bones = self.mmdData.bones
-  #   rigidObjects = []
-  #   rigidShapes = []
-  #   for i in range(len(rigids)):
-  #     # rigidbodyをくっつけるためのオブジェクトを生成する
-  #     rigidObj = maya.cmds.polyCube(name="rigid_" + self.nameDict[i])[0]
-  #     shape = self._initializePose(rigidObj, rigids[i])
-  #     self._initializeParams(shape, rigids[i].param)
-  #     self._initializeColider(shape, rigids[i])
-  #     self._constructConstraint(bones, rigidObj, shape, rigids[i], jointNames)
-  #     self._settingCollision(shape, rigids[i])
-
-  #     rigidObjects.append(rigidObj)
-  #     rigidShapes.append(shape)
-  #   return rigidObjects, rigidShapes
-
-
-  # def _setJointLimitation(self, constraint, minVector, maxVector, limitType, axis, i):
-  #   args = (constraint, limitType, axis)
-  #   minValue = minVector[i] #self._convertCoordinate(minVector[i], limitType, axis)
-  #   maxValue = maxVector[i] #self._convertCoordinate(maxVector[i], limitType, axis)
-
-  #   if minVector[i] > maxVector[i]:
-  #     maya.cmds.setAttr("%s.%sConstraint%s" % args, 0)
-  #   if minVector[i] == 0.0 and maxVector[i] == 0.0:
-  #     maya.cmds.setAttr("%s.%sConstraint%s" % args, 1)
-  #   else:
-  #     maya.cmds.setAttr("%s.%sConstraint%s" % args, 2)
-  #     maya.cmds.setAttr("%s.%sConstraintMin%s" % args, minValue)
-  #     maya.cmds.setAttr("%s.%sConstraintMax%s" % args, maxValue)
-
-
-  # def _setSpringLimitation(self, constraint, limitVector, limitType, axis, i):
-  #   limitValue = limitVector[i] #self._convertCoordinate(limitVector[i], limitType, axis)
-  #   args = (constraint, limitType, axis)
-  #   maya.cmds.setAttr("%s.%sSpringEnabled%s" % args, 1)
-  #   maya.cmds.setAttr("%s.%sSpringStiffness%s" % args, limitValue)
-
-  # def _constraintJoint(self, joint, rigidShapes):
-  #   ai = joint.rigidbody_index_a
-  #   bi = joint.rigidbody_index_b
-  #   maya.cmds.select(rigidShapes[ai])
-  #   maya.cmds.select(rigidShapes[bi], tgl=True)
-  #   constraint = bullet.RigidBodyConstraint.CreateRigidBodyConstraint().executeCommandCB()[0]
-  #   maya.cmds.setAttr("%s.constraintType" % constraint, 5)
-
-  #   axis = ["X", "Y", "Z"]
-  #   for i in range(3):
-  #     self._setJointLimitation(constraint, joint.translation_limit_min, joint.translation_limit_max, "linear", axis[i], i)
-  #     self._setJointLimitation(constraint, joint.rotation_limit_min, joint.rotation_limit_max, "angular", axis[i], i)
-  #     self._setSpringLimitation(constraint, joint.spring_constant_translation, "linear", axis[i], i)
-  #     self._setSpringLimitation(constraint, joint.spring_constant_rotation, "angular", axis[i], i)
-
-
-  # def _createJoint(self, rigidShapes):
-  #   joints = self.mmdData.joints
-  #   for i in range(len(joints)):
-  #     self._constraintJoint(joints[i], rigidShapes)
-
-
-  # def generate(self, jointNames):
-  #   rigidObjects, rigidShapes = self._createRigidObjects(jointNames)
-  #   self._createJoint(rigidShapes)
