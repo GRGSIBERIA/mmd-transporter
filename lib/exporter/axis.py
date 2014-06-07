faed1d829c0ffb9d70b4cc7545193a8ae3626004
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-06-08 03:04:17 +0900

    もうちょっと書く

--- lib/exporter/axis.py
+++ lib/exporter/axis.py
@@ -20,21 +20,25 @@ class AxisBase:
     return [x, y, z]
 
 
-  def __init__(self):
-    pass
+  def getMatrix(self, boneName):
+    orient = self.getJointOrients(boneName)
+    rotation = maya.OpenMaya.MEulerRotation(orient[0], orient[1], orient[2])
+    return rotation.asMatrix()
+
+
+  def getAxis(self, matrix, col):
+    return maya.OpenMaya.MVector(matrix(col, 0), matrix(col, 1), -matrix(col, 2))
+
+
+  def __init__(self, boneNames):
+    self.boneNames = boneNames
 
 
 class FixedAxis(AxisBase):
 
   def _calcAxis(self, boneName):
-    orient = self.getJointOrients(boneName)
-    rotation = maya.OpenMaya.MEulerRotation(orient[0], orient[1], orient[2])
-    quaternion = rotation.asQuaternion()
-    vector = maya.OpenMaya.MVector(0, 0, 0)
-    double = maya.OpenMaya.MScriptUtil()
-    double.createFromDouble(0.0)
-    pointer = double.asDoublePtr()
-    vector, angle = quaternion.getAxisAngle(vector, pointer) # 引数がMVector&, double&なので渡し方がわからん
+    matrix = self.getMatrix(boneName)
+    vector = self.getAxis(matrix, 0)
     return vector
 
 
@@ -43,35 +47,41 @@ class FixedAxis(AxisBase):
     for i in range(len(self.boneNames)):
       boneName = self.boneNames[i]
       if self.enables[i]:
-        axis = self._calcAxis(boneNames[i])
-        fixedAxises.append(vector)
+        axis = self._calcAxis(boneName)
+        fixedAxises.append(axis)
       else:
         fixedAxises.append(None)
     return fixedAxises
       
 
   def __init__(self, boneNames):
-    AxisBase.__init__(self)
-    self.boneNames = boneNames
+    AxisBase.__init__(self, boneNames)
     self.enables = getEnableFlagList(self.boneNames, "FixedAxis")
     self.fixedAxises = self._getFixedAxis()
 
 
 class LocalAxis(AxisBase):
 
+  def _calcLocal(self, boneName):
+    matrix = self.getMatrix(boneName)
+    vectorX = self.getAxis(matrix, 0)
+    vectorZ = self.getAxis(matrix, 2)
+    return vectorX, vectorZ
+    
+
+  def _getLocalAxis(self):
+    localAxises = []
+    for i in range(len(self.boneNames)):
+      boneName = self.boneNames[i]
+      if self.enables[i]:
+        axis = self._calcLocal(boneName)
+        localAxises.append(axis)
+      else:
+        localAxises.append(None)
+    return localAxises
+
+
   def __init__(self, boneNames):
-    AxisBase.__init__(self)
-    self.boneNames = boneNames
+    AxisBase.__init__(self, boneNames)
     self.enables = getEnableFlagList(self.boneNames, "LocalAxis")
 
-#import maya.cmds
-#import maya.OpenMaya
-#
-#rot = maya.OpenMaya.MEulerRotation(0, 0, 0)
-#qua = rot.asQuaternion()
-#vec = maya.OpenMaya.MVector(0, 0, 0)
-#dou = maya.OpenMaya.MScriptUtil()
-#dou.createFromDouble(0.0)
-#ptr = dou.asDoublePtr()
-#res = qua.getAxisAngle(vec, ptr)
-#print vec
