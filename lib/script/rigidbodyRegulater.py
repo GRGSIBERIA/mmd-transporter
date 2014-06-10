de975606ca0a445052d379b97b8a10e1250d0b73
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-06-11 04:39:15 +0900

    hoge

--- lib/script/rigidbodyRegulater.py
+++ lib/script/rigidbodyRegulater.py
@@ -1,7 +1,7 @@
 import maya.cmds
+import string
 
-
-class rigidbodyRegulaterWindow:
+class RigidbodyRegulaterWindow:
 
   def _getRigidbodyGroup(self):
     selection = maya.cmds.ls(sl=True)
@@ -33,3 +33,73 @@ class rigidbodyRegulaterWindow:
     self.rigidbodyGroup = self._getRigidbodyGroup()
     self.rigidbodyShapes = self._listingShapes()
     self.colliders = self._listingColliders()
+
+
+  def _createDefaultName(self, label):
+    attrName = label.replace(" ", "")
+    return "default" + attrName
+
+
+  def _createAttributeName(self, label):
+    try:
+      strs = label.split(" ")   # 空白文字を削除して、先頭文字を小文字にする
+      strs[0] = strs[0].lower()
+      attribute = strs[0] + strs[1]
+    except:
+      return label.lower()
+    return attribute
+
+
+  def _changeFloatField(self, defaultName, attributeName, value):
+    for collider in self.colliders:
+      try:
+        defaultValue = maya.cmds.getAttr("%s.%s" % (collider, defaultName))
+        maya.cmds.setAttr("%s.%s" % (collider, attributeName), defaultValue * value)
+      except:
+        pass  # lengthなど存在しないColliderもあるのでパス
+
+
+  def _showLine(self, label):
+    width = 150
+    changeMehod = lambda *args:self._changeFloatField(defaultName, attributeName, args[0])
+    defaultName = self._createDefaultName(label)
+    attributeName = self._createAttributeName(label)
+    maya.cmds.rowLayout(numberOfColumns=2, columnWidth2=(width, 200))
+    maya.cmds.text(label=label,
+      align="right",
+      width=width-20)
+    maya.cmds.floatField(v=1.0,
+      changeCommand=changeMehod,
+      dragCommand=changeMehod)
+    maya.cmds.setParent("..")
+
+
+  def _layout(self):
+    maya.cmds.columnLayout()
+
+    maya.cmds.frameLayout(l="Rigidbody Properties")
+    self._showLine("Mass")
+    self._showLine("Linear Damping")
+    self._showLine("Angular Damping")
+    self._showLine("Friction")
+    self._showLine("Restitution")
+    
+    maya.cmds.frameLayout(l="Collider Properties")
+    self._showLine("Length")
+    self._showLine("Radius")
+    self._showLine("ExtentsX")
+    self._showLine("ExtentsY")
+    self._showLine("ExtentsZ")
+
+    # JointConstraintの設定をする
+
+  def show(self):
+    window = maya.cmds.window(t="Rigidbody Regulater", w=400, h=300)
+
+    self._layout()
+
+    maya.cmds.showWindow(window)
+
+maya.cmds.select("rigidbodies")
+w = RigidbodyRegulaterWindow()
+w.show()
