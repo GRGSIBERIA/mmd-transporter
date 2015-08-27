cc4a23ac6be73f66f8701fc0ff23497283123118
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-08-27 20:18:33 +0900

    試す

---
+++ mmd-transporter/main.py
@@ -0,0 +1,9 @@
+﻿#-*- encoding: utf-8
+# Mayaを起動時に以下のコードを実行する
+# import maya.cmds as cmds
+# try: cmds.commandPort(name="127.0.0.1:6000", close=True, echoOutput=True)
+# except: pass
+# cmds.commandPort(name="127.0.0.1:6000", echoOutput=True)
+# 実行したいコードを選択して，ToMayaでMayaに実行コードを転送できる
+
+import maya.cmds
