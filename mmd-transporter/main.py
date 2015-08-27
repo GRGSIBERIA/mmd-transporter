e49c276a0c6943c0014f0bbb43d85aa8820830cd
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-08-27 20:33:07 +0900

    とりま，途中まで

--- mmd-transporter/main.py
+++ mmd-transporter/main.py
@@ -6,4 +6,33 @@
 # cmds.commandPort(name="127.0.0.1:6000", echoOutput=True)
 # 実行したいコードを選択して，ToMayaでMayaに実行コードを転送できる
 
+import sys
 import maya.cmds
+import maya.OpenMaya as OM
+import maya.OpenMayaMPx as MPx;
+
+kNodeName = "MMD Transporter"
+kNodeId = OM.MTypeId(0x3939)
+
+def nodeCreator():
+    pass
+
+def nodeInitializer():
+    pass
+
+# プラグインの初期化
+def initializePlugin(obj):
+    plugin = MPx.MFnPlugin(obj)
+    try:
+        plugin.registerNode(kNodeName, kNodeId, nodeCreator, nodeInitializer)
+    except:
+        sys.stderr.write('Failed to register node: %s' % (kNodeName))
+        raise
+
+# プラグインの解放
+def uninitializePlugin(obj):
+    plugin = MPx.MFnPlugin(obj)
+    try:
+        plugin.deregisterNode(kNodeId)
+    except:
+        sys.stderr.write('Failed to deregister node: %s' % (kNodeName))
