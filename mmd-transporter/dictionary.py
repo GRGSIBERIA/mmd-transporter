3ad593b3be7f60f82c7be9b4a515f529e30ed0cc
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-01 17:17:14 +0900

    辞書クラスを作成

---
+++ mmd-transporter/dictionary.py
@@ -0,0 +1,17 @@
+﻿#-*- encoding: utf-8
+
+from hepburn import *
+
+class Dictionary:
+    def __getNames(datas):
+        dict = []
+        for data in datas:
+            dict.append(hepburn(data.name))
+        return dict
+
+    def __init__(self, mmdData):
+        self.materials  = __getNames(mmdData.materials)
+        self.bones      = __getNames(mmdData.bones)
+        self.joints     = __getNames(mmdData.joints)
+        self.rigidbodies= __getNames(mmdData.rigidbodies)
+        self.morphs     = __getNames(mmdData.morphs)
