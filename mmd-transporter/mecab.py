d0a1f584a19f0f5ac4acbe2975695ead02eb72d0
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-04 17:59:49 +0900

    Maya上で動作できるように改定

--- mmd-transporter/mecab.py
+++ mmd-transporter/mecab.py
@@ -2,11 +2,16 @@
 
 import os
 import subprocess
+import locale
+from subprocess import PIPE, Popen
 
 class MeCabBinding:
     def __init__(self):
-        self.mecab_path = os.environ.get("MECAB_PATH") + "bin\\mecab.exe"
+        self.mecab_path = os.environ.get("MECAB_PATH") + "\\bin\\mecab.exe"
         print self.mecab_path
 
     def doHepburn(self, strings):
-        return subprocess.check_output("echo %s | %s -Oyomi" % (strings, self.mecab_path), shell=True)
+        command = u"echo %s | \"%s\" -Oyomi" % (strings, self.mecab_path)
+        process = Popen(command.encode(locale.getpreferredencoding()), stdin=PIPE, stdout=PIPE, shell=True)
+        (result, errcode) = process.communicate()
+        return result
